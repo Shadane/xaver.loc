285cf2e131de3d1c8c569b0e14a655b2baaad7af
Alex Zot <shadane@mail.com>
2015-02-22 17:01:53 +0600

    22.02.2015 17:01

--- L7HW1.php
+++ L7HW1.php
@@ -3,25 +3,24 @@ error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 ini_set('display_errors', 1);
 header('Content-type: text/html; charset=utf-8');
 
+function adsSave($ads_container, $sent_entry){  
+    if ( isset( $sent_entry['return_id'] )  &&  is_numeric( $sent_entry['return_id'] ) ){
+            $ads_container[$sent_entry['return_id']]=$sent_entry; 
+    }else{
+            $ads_container[]=$sent_entry;
+    }
+    return $ads_container;
+}
 
-//блоки
-function button_processor($mode){
-    global $ads_container,$showform_params;
-    
-    if (isset($_POST['main_form_submit'])) {    //send button
-          if (($_POST['title'])){     
-                if ($mode == 'edit'){
-                        $ads_container[$_POST['return_id']]=$_POST; 
-                }else{
-                        $ads_container[]=$_POST;
-                }
-          }else{
-                $showform_params['notice_field_is_empty']='Введите название';
-          }
-    }elseif (isset($_GET['delentry'])) {           //delete button
-           unset($ads_container[$_GET['delentry']]); 
-    }elseif (isset($_GET['formreturn'])) {        //return_values button
-            $return = $ads_container[$_GET['formreturn']];
+
+function adsDelete($ads_container, $delete_id){ //обосновано ли использование такой функции?)
+    unset($ads_container[$delete_id]); 
+    return $ads_container;
+}
+
+
+function adsReturn($ads_container, $showform_params, $return_id){
+            $return = $ads_container[$return_id];
             $showform_params = array(
                    'return_private' => "",
                    'return_company' =>"",
@@ -34,21 +33,22 @@ function button_processor($mode){
                    'returndescription' => $return['description'],
                    'returnprice' => $return['price'],
                    'return_id' => $_GET['formreturn'],
-                   'notice_field_is_empty'=> ""
+                   'notice_title_is_empty'=> ""
                                 );
             if ($return['private']=='1'){
                     $showform_params['return_private'] = 'checked=""';
             }else{
                     $showform_params['return_company'] = 'checked=""';
             }
-            $showform_params['return_send_email'] = (isset($return['allow_mails'])) ? 'checked=""' : '';//закончили заполнение массива
+            $showform_params['return_send_email'] = (isset($return['allow_mails'])) ? 'checked=""' : '';
+            
+            return $showform_params;
 }
-}
 
 
 
-function include_city_options(){
-    global $cities,$showform_params;
+
+function include_city_options($showform_params, $cities){
     foreach ($cities as $key => $value) {
         $selected = ($key == $showform_params["city"]) ? 'selected=""' : '';
         echo ' <option data-coords=",,"' . $selected . ' value="' . $key . '">' . $value . '</option>';
@@ -57,8 +57,7 @@ function include_city_options(){
 
 
 
-function include_category_options(){
-    global $categories,$showform_params;
+function include_category_options($showform_params, $categories){
     foreach ($categories as $category => $subarray) { //цикл для вывода категорий и субкатерогий в выпадающем списке
         echo '<optgroup label="' . $category . '">';
         foreach ($subarray as $key => $subcatname) {
@@ -68,11 +67,7 @@ function include_category_options(){
     }
 }
 
-
-
-function show_table_contents(){ //блок вывода таблицы по шаблону 
-// Вывод таблицы из массива.
-   global $ads_container;
+function showTable($ads_container){
     if (!empty($ads_container)) {     
         foreach ($ads_container as $key => $array) {
                 echo '<tr>';
@@ -81,87 +76,18 @@ function show_table_contents(){ //блок вывода таблицы по ша
                 echo '<td>  |  ' . $array['seller_name'] . '</td>';
                 echo '<td>  |  <a href="?delentry=' . $key . '">Удалить</a> |</td>';
                 echo '</tr>';
-            }
         }
-    
-    ?>
-                                      </table>
-    <?php
+    }
 }
 
-
-
-function show_form($showform_params) { 
-?>
-<html>
-    <head>  
-        <style>  
-        div { width: 800px;}
-        </style>  
-    </head>  
-    <form  method="post">
-        <div style="margin-left:220px;margin-top:10px"> 
-            <label><input type="radio" <?php echo $showform_params['return_private'];?> value="1" name="private">Частное лицо</label>
-            <label style="margin-left:20px"><input type="radio" value="0" <?php echo $showform_params['return_company'];?> name="private">Компания</label>
-        </div>
-        <div style="margin-left:60px;margin-top:10px"> 
-            <label><b>Ваше имя</b></label>
-            <input style="margin-left:85px; width:230px" type="text" maxlength="40" value="<?php echo  $showform_params["namereturn"] ?>" name="seller_name">
-        </div>
-        <div style="margin-left:60px;  margin-top:10px"> 
-            <label>Электронная почта</label>
-            <input style="margin-left:27px; width:230px;" type="text"  value="<?php echo $showform_params["email_return"]; ?> " name="email" id="fld_email">
-        </div>
-        <div style="margin-left:217px;  margin-top:10px">
-            <label><input type="checkbox"<?php echo $showform_params["return_send_email"]; ?>  value="1" name="allow_mails" id="allow_mails" class="form-input-checkbox">
-                Я не хочу получать вопросы по объявлению по e-mail</label> 
-        </div>
-        <div style="margin-left:60px;  margin-top:10px"> 
-            <label>Номер телефона</label> 
-            <input style="margin-left:46px; width:230px" type="text"  value="<?php echo $showform_params["phonereturn"]; ?>" name="phone">
-        </div>
-        <div style="margin-left:60px;  margin-top:10px"> 
-            <label >Город</label> 
-            <select style="margin-left:118px; width:230px;height:22px" title="Выберите Ваш город" name="location_id"> 
-                <option>-- Выберите город --</option>
-                <option disabled="disabled">-- Города --</option>
-                <?php include_city_options(); ?>
-                <option>Выбрать другой...</option>
-            </select>
-        </div>
-        <div style="margin-left:60px;  margin-top:10px"> 
-            <label for="fld_category_id" class="form-label">Категория</label> 
-            <select style="margin-left:89px; width:230px;height:22px" name="category_id">
-                <option value="">-- Выберите категорию --</option>
-                <?php include_category_options();?>
-            </select> 
-        </div>
-        <div style="margin-left:60px;  margin-top:10px">
-            <label>Название объявления</label> 
-            <input style="margin-left:12px; width:230px;" type="text" maxlength="50" placeholder="<?php echo $showform_params['notice_field_is_empty']. '" value="' . $showform_params["returntitle"]; ?>" name="title">
-        </div>
-        <div style="margin-left:60px;  margin-top:10px"> 
-            <label style="position:absolute">Описание объявления</label>
-            <textarea style="margin-left:162px; width:230px;height:70px;" maxlength="3000" name="description" ><?php echo $showform_params["returndescription"]; ?></textarea></div>
-        <div style="margin-left:60px;  margin-top:10px"> 
-            <label >Цена</label>
-            <input style="margin-left:124px; width:230px" type="text" maxlength="9"  value="<?php echo $showform_params["returnprice"]; ?> " name="price" >
-            <div>
-                <div style="margin-left:161px;  margin-top:10px"> 
-                    <input type="hidden" value="<?php echo $showform_params["return_id"]; ?>" name="return_id" >
-                <input style="height:30px;font-weight: 700;color:white;border-radius: 3px;background: rgb(64,199,129);box-shadow: 0 -3px rgb(53,167,110) inset;transition: 0.2s;" type="submit" value="Отправить" name="main_form_submit"  >
-                </div>
-            </div>
-    </form>
-    <table method="post" style="border: 1px solid black; margin-top:30px;margin-left: 30px">
-            <tr>
-                <td> |  Название объявления </td>
-                <td>  |  Цена </td>
-                <td>  |  Имя </td>
-                <td>  |  Удалить | </td>
-            </tr>
-        </div>
-        <?php show_table_contents();
+function showForm($ads_container, $showform_params, $cities, $categories){ //блок вывода таблицы по шаблону 
+          include './adsform.html'; //тут получилось, include_category_options, include_city_options - вызываются из файла.
+          showTable($ads_container);
+ ?>
+           </table>
+           </body>
+           </html>
+<?php    
 }
 
 
@@ -178,7 +104,7 @@ $showform_params = array(
                         'returntitle' => "",
                         'returndescription' => "",
                         'returnprice' => "0",
-                        'notice_field_is_empty'=> "",
+                        'notice_title_is_empty'=> "",
                         'return_id' => ""
                            );
 
@@ -219,13 +145,17 @@ $ads_save_checker=$ads_container;
 
 
   //button processing
-  if (isset($_POST['return_id'])  &&  is_numeric($_POST['return_id'])){
-        //edit_MODE
-      button_processor('edit');
-  }else{ 
-    //normal_MODE
-      button_processor('normal');
-  }  
+   if ( isset($_POST['main_form_submit']) ) {    //send button
+          if ( $_POST['title'] ){
+             $ads_container = adsSave( $ads_container, $_POST );
+          }else{
+                $showform_params['notice_title_is_empty']='Введите название';
+          }
+   }elseif ( isset($_GET['delentry']) ) {           //delete button
+         $ads_container = adsDelete( $ads_container, $_GET['delentry'] );
+   }elseif ( isset($_GET['formreturn']) ) {         //return values to form button
+         $showform_params = adsReturn( $ads_container, $showform_params, $_GET['formreturn'] );
+   }
   
   
   //saving to file
@@ -233,5 +163,5 @@ $ads_save_checker=$ads_container;
   file_put_contents('./adsholder.txt', serialize($ads_container));
    }
    //конец логики, вывод
-  show_form($showform_params); 
+   showForm($ads_container, $showform_params, $cities, $categories);
   ?>
